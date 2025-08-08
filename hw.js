4aff31a0b502b4fc1fc1b121a074ff35e18d9ec2
NIKITAPRO965 <nikita.pro.965@gmail.com>
2025-08-08 11:11:59 +0300

    .

--- hw.js
+++ hw.js
@@ -1,33 +1,33 @@
 //Створіть змінну, що симулює вибір варіанта зі списку. Виводьте повідомлення залежно від обраного варіанта: "Кава", "Чай" або "Сік". Використайте switch
-// const choice = Number(prompt("Виберіть варіант зі списку: 1-Кава, 2-Чай, 3-Сік."));
-// switch (choice) {
-//     case 1:
-//     console.log(`Кава`);
-//         break;
-//     case 2:
-//     console.log(`Чай`);
-//         break;
-//     case 3:
-//     console.log(`Сік`);
-//         break;
-//     default:
-//     console.log(`Нажаль такого у нас нема`);
-//         break;
-// }
+const choice = prompt("Виберіть варіант зі списку: Кава, Чай, Сік.").trim().toLowerCase();
+switch (choice) {
+    case "кава":
+    console.log(`Кава`);
+        break;
+    case "чай":
+    console.log(`Чай`);
+        break;
+    case "сік":
+    console.log(`Сік`);
+        break;
+    default:
+    console.log(`Нажаль такого у нас нема`);
+        break;
+}
 
 
 //Створіть змінну для зберігання введеного рядка, який може бути днем тижня. Якщо це робочий день — виведіть повідомлення про робочий день, якщо вихідний — про вихідний.
-const day = prompt("Вкажіть день тижня").trim().toLocaleLowerCase();
+const day = prompt("Вкажіть день тижня").trim().toLowerCase();
 switch (day) {
-  case "Понеділок":
-  case "Вівторок":
-  case "Середа":
-  case "Четвер":
-  case "П’ятниця":
+  case "понеділок":
+  case "вівторок":
+  case "середа":
+  case "четвер":
+  case "п’ятниця":
     console.log("Це робочий день.");
     break;
-  case "Субота":
-  case "Неділя":
+  case "субота":
+  case "неділя":
     console.log("Це вихідний день.");
     break;
   default:
@@ -39,36 +39,36 @@ switch (day) {
 
 
 //Створіть змінну для зберігання номера місяця. За номером місяця визначайте пору року і виводьте відповідне повідомлення.
-// const month = Number(prompt("Вкажіть номер місяця:"));
-// switch (month) {
-//   case 12:
-//   case 1:
-//   case 2:
-//     console.log("Зима");
-//     break;
-//   case 3:
-//   case 4:
-//   case 5:
-//     console.log("Весна");
-//     break;
-//   case 6:
-//   case 7:
-//   case 8:
-//     console.log("Літо");
-//     break;
-//   case 9:
-//   case 10:
-//   case 11:
-//     console.log("Осінь");
-//     break;
-//   default:
-//     console.log("Неправильний номер місяця.");
-// }
+const month = Number(prompt("Вкажіть номер місяця:"));
+switch (month) {
+  case 12:
+  case 1:
+  case 2:
+    console.log("Зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Літо");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("Осінь");
+    break;
+  default:
+    console.log("Неправильний номер місяця.");
+}
 
 
 
 //Створіть змінну для зберігання назви кольору. Виводьте повідомлення відповідно до вибраного кольору: якщо "червоний" — "стоп", "зелений" — "йти", "жовтий" — "чекати".
-const color = prompt("Вкажіть колір: червоний, зелений, жовтий.").trim().toLocaleLowerCase();
+const color = prompt("Вкажіть колір: червоний, зелений, жовтий.").trim().toLowerCase();
 switch (color) {
   case "червоний":
     console.log("стоп");
@@ -87,28 +87,27 @@ switch (color) {
 
 
 //Створіть змінні для зберігання двох чисел та оператора (як у списку select). Виконайте відповідну операцію та виведіть результат. У випадку ділення на нуль — виведіть попередження.
-// const a = 6;
-// const b = 5;
-// const op = prompt("Введіть знаки для прикладу 6 ? 5 = ?: +, -, *, /.");
+const num1 = Number(prompt("Введіть перше число"));
+const num2 = Number(prompt("Введіть друге число"));
+const operator = prompt("Введіть оператор");
+let result;
 
-// switch (op) {
-//   case "+":
-//     console.log(a + b);
-//     break;
-//   case "-":
-//     console.log(a - b);
-//     break;
-//   case "*":
-//     console.log(a * b);
-//     break;
-//   case "/":
-//     console.log(a / b);
-//     break;
-//   default:
-//     console.log("Невірна операція");
-// }
-
-
-
-
+if (operator === "+") {
+    result = num1 + num2;
+} else if (operator === "-") {
+    result = num1 - num2;
+} else if (operator === "*") {
+    result = num1 * num2;
+} else if (operator === "/") {
+    if (num2 === 0) {
+        console.log("Помилка: ділення на нуль неможливе!");
+    } else {
+        result = num1 / num2;
+    }
+} else {
+    console.log("Невідомий оператор!");
+}
+if (result !== undefined) {
+    console.log("Результат:", result);
+}
 
